7e92d1b5afb23343b7d9f555bb14f263ea1d6f9e
Lado Lomidze <lado.lomidze@gmail.com>
2021-07-07 17:27:44 +0200

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-// Please run your solution from this file
+import { prepareData, renderData } from "./solution";
 
-console.log("Hello from %csrc/index.js", "font-weight:bold");
+(() => {
+  // Yes, I could also have used axios and/or async/await syntax here, but why?
+  // And you might even think, that this could have been done without IIFE also.
+  fetch("https://api.spacexdata.com/v3/launches/past")
+    .then((res) => res.json())
+    .then(prepareData) // Same as `.then(data => prepareData(data))`
+    .then(renderData)
+    // Yeah, old good `alert`.
+    .catch(() => alert("Sorry, can't display list of SpaceX launches."));
+})();
--- src/solution.js
+++ src/solution.js
@@ -1,8 +1,63 @@
-// Please implement your solution in this file
+import { parseISO, getYear } from "date-fns";
 
-const noop = () => {};
+const renderData = (data) => {
+  // This could have been an arrow function without brackets.
+  // But, honestly I did not like, how prettier formatted it.
+  document.querySelector("#out").innerHTML = JSON.stringify(data, null, 2);
+};
+
+const prepareData = (data) => {
+  const byYear2018 = ({ launch_date_utc }) => {
+    // Initially, for UTC dates I wanted to use Date.parse() method.
+    // But MDN says that it's not recommended using it,
+    // because of the some parsing implementations in JS.
+    // So I decided to use date-fns library, which is basically
+    // lightweight and more functional alternative to moment.js.
+    return getYear(parseISO(launch_date_utc)) === 2018;
+  };
+
+  const byNASA = ({ rocket }) => {
+    // At first I thought that simple `array.includes` method would do the trick for finding in nested array,
+    // but when I got stuck, after a research, I ended up using solution found here: https://stackoverflow.com/a/40025777/313750
+    return rocket.second_stage.payloads.find(({ customers }) =>
+      customers.some((customer) => customer.includes("NASA"))
+    );
+  };
+
+  const byPayloadsCount = (a, b) => {
+    const payloadsCountA = a.rocket.second_stage.payloads.length;
+    const payloadsCountB = b.rocket.second_stage.payloads.length;
+
+    const launchDateA = a.launch_date_unix;
+    const launchDateB = b.launch_date_unix;
+
+    // First sort by number of payloads, then by launch date in DESC.
+    if (payloadsCountA < payloadsCountB || launchDateA < launchDateB) return 1;
+    if (payloadsCountA > payloadsCountB || launchDateA > launchDateB) return -1;
+    return 0;
+  };
+
+  // Honestly, I suck at naming things, so `toRender` was best, what I could come up with.
+  const toRender = ({ flight_number, mission_name, rocket }) => ({
+    flight_number,
+    mission_name,
+    payloads_count: rocket.second_stage.payloads.length,
+  });
+
+  // Yes, a lot of variables and functions were introduced above, I know.
+  // But I think code is for humans and I really like, how the next few lines look like.
+  return (
+    data
+      // Yes, I know, that using `.filter` method twice adds extra iteration, which can be less optimized.
+      // So, here's an alternative way of doing it: `.filter((launch) => byYear2018(launch) && byNASA(launch))`
+      .filter(byYear2018)
+      .filter(byNASA)
+      .sort(byPayloadsCount)
+      .map(toRender)
+  );
+};
 
 module.exports = {
-  prepareData: noop,
-  renderData: noop
+  prepareData,
+  renderData,
 };
